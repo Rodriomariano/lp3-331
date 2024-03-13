669b0cd17f8883c1fd2774c6db9a021b4bd96c0d
Rodrigo <rodrigo.mariano@aluno.ifsp.edu.br>
2024-03-13 17:51:15 -0300

    semana04

---
+++ semana04/exercicios.py
@@ -0,0 +1,26 @@
+# Ex01
+
+numero = int(input('digite um numero:'))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print('antecessor', antecessor)
+print('sucessor', sucessor)
+
+# Ex02
+
+numero1 = int(input('digite o primeiro numero:'))
+numero2 = int(input('digite o segundo numero:'))
+numero3 = int(input('digite o terceiro numero:'))
+
+media = (numero1 + numero2 + numero3) / 3
+
+print('media', media)
+
+# Ex03
+
+
+
+    
+
